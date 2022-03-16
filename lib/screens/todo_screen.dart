e97814feea0a59d967c5a5994e928b1df7cd4837
Nudlaa <bartak.ra@gmail.com>
2022-03-16 15:22:04 +0100

    Zkouška přidání textfieldu - nefunguje

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -10,11 +10,18 @@ class TodoScreen extends StatefulWidget {
   @override
   State<TodoScreen> createState() => _TodoScreenState();
 }
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "neco",
+    );
+}
 
 class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white38,
       appBar: AppBar(
         title: const Text("TodoScreen"),
       ),
@@ -25,6 +32,60 @@ class _TodoScreenState extends State<TodoScreen> {
             TodoItem(title:"vec1"),
             TodoItem(title:"vec2"),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Thanks!'),
+                  content: Text(
+                      'You typed "$value", which has length ${value.characters.length}.'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
         ),
       ),
     );
